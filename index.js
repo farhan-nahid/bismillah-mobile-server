ba64c034335d4ae030c12f69e329c81cf9dca46e
farhan-nahid <nahidahmed9933@gmail.com>
2021-04-01 00:40:15 +0600

    load information with mongodb

--- index.js
+++ index.js
@@ -11,14 +11,36 @@ app.use(cors())
 app.use(bodyParser.json())
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2xoju.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const MobileCollection = client.db("bismillahMobileZone").collection("mobile");
+  const mobileCollection = client.db("bismillahMobileZone").collection("mobile");
   
-    console.log('database');
+    app.post('/adminAddMobile', (req,res)=>{
+      const newMobile = req.body;
+     // console.log('Adding mobile', newMobile);
+      mobileCollection.insertOne(newMobile)
+      .then(result =>{
+        console.log('insertedCount',result.insertedCount);
+        res.send(result.insertedCount > 0)
+      })
+    })
+    
+    app.get('/mobileItems', (req, res)=>{
+      mobileCollection.find()
+      .toArray((err, mobileItems)=>{
+        res.send( mobileItems);
+      })
+
+    })
+      // app.get('/mobileItems/:_id:objectId', (req, res)=>{
+      //  console.log(req.params.id);
+      //  mobileCollection.find({req.params.id})
+      //    .toArray((err, mobileItems)=>{
+      //   res.send( mobileItems);
+      //   })
+
+  
 });
 
 
